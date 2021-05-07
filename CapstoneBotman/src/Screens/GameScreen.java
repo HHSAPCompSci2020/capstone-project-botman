6d959c02294674f7662ebcdeec045a7331535094
Giantpizzahead <43867185+Giantpizzahead@users.noreply.github.com>
2021-05-06 20:10:39 -0700

    DrawingSurface and Screen

--- CapstoneBotman/src/Screens/GameScreen.java
+++ CapstoneBotman/src/Screens/GameScreen.java
@@ -1,5 +1,29 @@
 package Screens;
 
-public class GameScreen {
+import Main.DrawingSurface;
+import Sprites.Player;
+
+public class GameScreen extends Screen {
+	
+	private DrawingSurface surface;
+	
+	private Player runner;
+	private Player hunter;
+
+	public GameScreen(int width, int height, DrawingSurface surface) {
+		super(width, height);
+		this.surface = surface;
+	}
+	
+	@Override
+	public void draw() {
+		surface.pushStyle();
+		surface.background(255, 200, 255);
+		
+		surface.fill(0, 100, 0);
+		surface.text("<Insert game here>", 200, 200);
+		
+		surface.popStyle();
+	}
 	
 }
