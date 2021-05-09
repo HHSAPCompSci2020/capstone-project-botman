b02b82a395d41a0d18dbf391d45975f2847c70a6
akumar200-hhs <69864290+akumar200-hhs@users.noreply.github.com>
2021-05-09 00:23:07 -0700

    Update MainScreen.java

--- CapstoneBotman/src/Screens/MainScreen.java
+++ CapstoneBotman/src/Screens/MainScreen.java
@@ -2,7 +2,9 @@ package Screens;
 
 import Main.DrawingSurface;
 import g4p_controls.GButton;
+import g4p_controls.GEvent;
 import g4p_controls.GLabel;
+import processing.core.PConstants;
 
 public class MainScreen extends Screen {
 
@@ -18,6 +20,51 @@ public class MainScreen extends Screen {
 		
 		x = 0;
 		y = 0;
+		
 	}
 	
+	public void setup() {
+		instructions = new GButton(surface, 90, 100, 200, 50, "Instrucitons");
+		play = new GButton(surface, 90, 175, 200, 50, "Play");
+		
+		//title.addEventHandler(instructions, "handleButtonEvents");
+		instructions.addEventHandler(this, "handleButtonEvents");
+		play.addEventHandler(this, "handleButtonEvents");
+	}
+	
+	public void draw() {
+		surface.pushStyle();
+		surface.background(0, 0, 0);
+		surface.fill(255, 255, 0);
+		
+		// draws title and buttons
+//		title = new GLabel(surface, 150, 50, 500, 50, "VALHUNT");
+//		title.setLocalColor(45, 500);
+		
+		// VALHUNT title drawn
+		surface.textAlign(PConstants.CENTER, PConstants.CENTER);
+		surface.textSize(50);
+		surface.text("VALHUNT", 200, 50);
+	}
+	
+	public void handleButtonEvents(GButton button, GEvent event) {
+		// if play is clicked, switch to instructions screen
+		if (button == instructions && event == GEvent.CLICKED) {
+			surface.switchScreen(1);
+			
+			// removes buttons
+			instructions.setVisible(false);
+			play.setVisible(false);
+		}
+		
+		// if play is clicked, switch to game screen
+		if (button == play && event == GEvent.CLICKED) {
+			surface.switchScreen(3);
+			
+			// removes buttons
+			instructions.setVisible(false);
+			play.setVisible(false);
+		}
+	}
+
 }
