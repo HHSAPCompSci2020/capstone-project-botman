f69b44710715ee99571b7f0ebeb925debc00a5b4
akumar200-hhs <69864290+akumar200-hhs@users.noreply.github.com>
2021-05-09 00:36:14 -0700

    added javadocs
    :)

--- CapstoneBotman/src/Screens/MainScreen.java
+++ CapstoneBotman/src/Screens/MainScreen.java
@@ -8,12 +8,43 @@ import processing.core.PConstants;
 
 public class MainScreen extends Screen {
 
-	private int x, y;
+	/**
+	 * X-coordinate of the top right of the screen
+	 */
+	private int x;
+	
+	/**
+	 * Y-coordinate of the top right of the screen
+	 */
+	private int y;
+	
+	/**
+	 * PApplet surface where the game is drawn
+	 */
 	private DrawingSurface surface;
 	
-	GButton instructions, play;
+	/**
+	 * Button that leads to the instructions screen
+	 */
+	GButton instructions;
+	
+	/**
+	 * Button that leads to the play screen
+	 */
+	GButton play;
+	
+	/**
+	 * Title that does not display anything currently
+	 */
 	GLabel title;
 	
+	/**
+	 * Sets up the main screen
+	 * 
+	 * @param width Initial width of the screen
+	 * @param height Initial height of the screen
+	 * @param surface PApplet where the screen is drawn
+	 */
 	public MainScreen(int width, int height, DrawingSurface surface) {
 		super(width, height);
 		this.surface = surface;
@@ -23,6 +54,9 @@ public class MainScreen extends Screen {
 		
 	}
 	
+	/**
+	 * Sets up instructions button and play button
+	 */
 	public void setup() {
 		instructions = new GButton(surface, 90, 100, 200, 50, "Instrucitons");
 		play = new GButton(surface, 90, 175, 200, 50, "Play");
@@ -35,6 +69,9 @@ public class MainScreen extends Screen {
 		play.setVisible(false);
 	}
 	
+	/**
+	 * Draws the title and buttons on the screen
+	 */
 	public void draw() {
 		surface.pushStyle();
 		surface.background(0, 0, 0);
@@ -47,12 +84,18 @@ public class MainScreen extends Screen {
 //		title = new GLabel(surface, 150, 50, 500, 50, "VALHUNT");
 //		title.setLocalColor(45, 500);
 		
-		// VALHUNT title drawn
+		// draws VALHUNT title
 		surface.textAlign(PConstants.CENTER, PConstants.CENTER);
 		surface.textSize(50);
 		surface.text("VALHUNT", 200, 50);
 	}
 	
+	/**
+	 * Handles the events and their consequences on different buttons
+	 * 
+	 * @param button Button that has the event performed on it
+	 * @param event Event which gets performed on the button
+	 */
 	public void handleButtonEvents(GButton button, GEvent event) {
 		// if play is clicked, switch to instructions screen
 		if (button == instructions && event == GEvent.CLICKED) {
